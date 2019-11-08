9013764b0bc1c7221339eaa36d8489e3328cf9de
Roy <symbiane@sina.com>
2019-11-08 18:49:40 +0800

    Updates

---
+++ precache-manifest.32d172093cfb11b14d4a2edb1b02a73f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e446da1846a77e825f6344b59432fff4",
+    "url": "/ilast-news/index.html"
+  },
+  {
+    "revision": "5c9113a43eae10bfb6f5",
+    "url": "/ilast-news/static/css/2.39de1a12.chunk.css"
+  },
+  {
+    "revision": "95d19944d80f10dc61f2",
+    "url": "/ilast-news/static/css/main.0e0ac034.chunk.css"
+  },
+  {
+    "revision": "5c9113a43eae10bfb6f5",
+    "url": "/ilast-news/static/js/2.5f07895d.chunk.js"
+  },
+  {
+    "revision": "95d19944d80f10dc61f2",
+    "url": "/ilast-news/static/js/main.fcfc458c.chunk.js"
+  },
+  {
+    "revision": "14dbd9f3ff7aa8b45918",
+    "url": "/ilast-news/static/js/runtime-main.89c341af.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ilast-news/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ilast-news/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ilast-news/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ilast-news/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ilast-news/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
